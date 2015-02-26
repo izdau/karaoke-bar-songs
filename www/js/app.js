c6721e1041672b5d701140089734a475adbbc779
izdau <baurzhan@outlook.com>
2015-02-26 17:07:37 +0600

    Updated the tabs section of the app

--- www/js/app.js
+++ www/js/app.js
@@ -6,97 +6,96 @@
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
 angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
-
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if (window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-    }
-    if (window.StatusBar) {
-      // org.apache.cordova.statusbar required
-      StatusBar.styleDefault();
-    }
-  });
-})
-
-.config(function($stateProvider, $urlRouterProvider) {
-
-  // Ionic uses AngularUI Router which uses the concept of states
-  // Learn more here: https://github.com/angular-ui/ui-router
-  // Set up the various states which the app can be in.
-  // Each state's controller can be found in controllers.js
-  $stateProvider
-
-  // setup an abstract state for the tabs directive
-    .state('tab', {
-    url: "/tab",
-    abstract: true,
-    templateUrl: "templates/tabs.html"
-  })
-
-  // Each tab has its own nav history stack:
-
-  .state('tab.dash', {
-    url: '/dash',
-    views: {
-      'tab-dash': {
-        templateUrl: 'templates/tab-dash.html',
-        controller: 'DashCtrl'
-      }
-    }
-  })
-
-  .state('tab.chats', {
-      url: '/chats',
-      views: {
-        'tab-chats': {
-          templateUrl: 'templates/tab-chats.html',
-          controller: 'ChatsCtrl'
-        }
-      }
+    .run(function ($ionicPlatform) {
+        $ionicPlatform.ready(function () {
+            // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+            // for form inputs)
+            if (window.cordova && window.cordova.plugins.Keyboard) {
+                cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+            }
+            if (window.StatusBar) {
+                // org.apache.cordova.statusbar required
+                StatusBar.styleDefault();
+            }
+        });
     })
-    .state('tab.chat-detail', {
-      url: '/chats/:chatId',
-      views: {
-        'tab-chats': {
-          templateUrl: 'templates/chat-detail.html',
-          controller: 'ChatDetailCtrl'
-        }
-      }
-    })
+    .config(function ($stateProvider, $urlRouterProvider, $ionicConfigProvider) {
+        // Set tab position to bottom (to handle Android tab top issue)
+        $ionicConfigProvider.tabs.position('bottom');
 
-  .state('tab.friends', {
-      url: '/friends',
-      views: {
-        'tab-friends': {
-          templateUrl: 'templates/tab-friends.html',
-          controller: 'FriendsCtrl'
-        }
-      }
-    })
-    .state('tab.friend-detail', {
-      url: '/friend/:friendId',
-      views: {
-        'tab-friends': {
-          templateUrl: 'templates/friend-detail.html',
-          controller: 'FriendDetailCtrl'
-        }
-      }
-    })
+        // Ionic uses AngularUI Router which uses the concept of states
+        // Set up the various states which the app can be in.
+        // Each state's controller can be found in controllers.js
+        $stateProvider.state('tab', {
+            url: "/tab",
+            abstract: true,
+            templateUrl: "templates/tabs.html"
+        })
+        .state('tab.all-songs', { // Each tab has its own nav history stack:
+            url: '/all-songs',
+            views: {
+                'tab-all-songs': {
+                    templateUrl: 'templates/tab-all-songs.html',
+                    controller: 'DashCtrl'
+                }
+            }
+        })
+        .state('tab.popular', {
+            url: '/popular',
+            views: {
+                'tab-popular': {
+                    templateUrl: 'templates/tab-popular.html',
+                    controller: 'ChatsCtrl'
+                }
+            }
+        })
+        .state('tab.chat-detail', {
+            url: '/chats/:chatId',
+            views: {
+                'tab-popular': {
+                    templateUrl: 'templates/chat-detail.html',
+                    controller: 'ChatDetailCtrl'
+                }
+            }
+        })
+        .state('tab.friends', {
+            url: '/filters',
+            views: {
+                'tab-filters': {
+                    templateUrl: 'templates/tab-filters.html',
+                    controller: 'FriendsCtrl'
+                }
+            }
+        })
+        .state('tab.friend-detail', {
+            url: '/friend/:friendId',
+            views: {
+                'tab-filters': {
+                    templateUrl: 'templates/friend-detail.html',
+                    controller: 'FriendDetailCtrl'
+                }
+            }
+        })
+        .state('tab.account', {
+            url: '/favorites',
+            views: {
+                'tab-favorites': {
+                    templateUrl: 'templates/tab-favorites.html',
+                    controller: 'AccountCtrl'
+                }
+            }
+        })
+        .state('tab.queue', {
+            url: '/queue',
+            views: {
+                'tab-queue': {
+                    templateUrl: 'templates/tab-queue.html',
+                    controller: 'AccountCtrl'
+                }
+            }
+        });
 
-  .state('tab.account', {
-    url: '/account',
-    views: {
-      'tab-account': {
-        templateUrl: 'templates/tab-account.html',
-        controller: 'AccountCtrl'
-      }
-    }
-  });
+        // if none of the above states are matched, use this as the fallback
+        $urlRouterProvider.otherwise('/tab/all-songs');
 
-  // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/tab/dash');
-
-});
+    });
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -8,19 +8,15 @@ angular.module('starter.controllers', [])
     Chats.remove(chat);
   }
 })
-
 .controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
   $scope.chat = Chats.get($stateParams.chatId);
 })
-
 .controller('FriendsCtrl', function($scope, Friends) {
   $scope.friends = Friends.all();
 })
-
 .controller('FriendDetailCtrl', function($scope, $stateParams, Friends) {
   $scope.friend = Friends.get($stateParams.friendId);
 })
-
 .controller('AccountCtrl', function($scope) {
   $scope.settings = {
     enableFriends: true
